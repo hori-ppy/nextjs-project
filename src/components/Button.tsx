e5afaa704ac228367aee285e61deaa62d68378f4
hori-ppy <>
2021-08-17 12:00:30 +0900

    CSS Modules 導入

---
+++ src/components/Button.tsx
@@ -0,0 +1,6 @@
+import React from "react"
+import styles from "./Button.module.scss"
+
+export const Button: React.FC = () => {
+    return <button className={styles.button}>ボタン１</button>
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
+import { Button } from "../components/Button";
+
 function HomePage() {
-    return <div>Welcome to Next.js!</div>;
+    return (
+        <>
+            <div>Welcome to Next.js!</div>
+            <Button />
+        </>
+    )
 }
 
 export default HomePage;
